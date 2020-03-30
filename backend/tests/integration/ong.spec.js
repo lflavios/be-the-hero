4f23ac7176f199a032054166a47bb767f56b5f50
Luís Flávio Seelig <luisseelig@tre-rs.jus.br>
2020-03-29 22:34:24 -0300

    Limpeza de código

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,31 @@
+const expect = require('expect');
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+  beforeEach( async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        nome: "APE",
+        email: "contato@spam.com.br",
+        whatsapp: "5199999999",
+        cidade: "Porto Alegre",
+        uf: "RS"
+      });
+
+    console.log(response.body);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+  });
+})
